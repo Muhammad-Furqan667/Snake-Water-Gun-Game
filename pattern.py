46b7bd6660e9c4639fd41ec0a5b8117b41550981
Muhammad Furqan <muhammadfurqan3624515@gmail.com>
2024-12-30 11:16:37 +0500

    Initial commit

---
+++ pattern.py
@@ -0,0 +1,24 @@
+
+#  Pattern = computer - you
+# else:    
+#     if(computer == -1 and you == 1): -2
+#         print("You Win!")
+
+#     elif(computer == -1 and you == 0): -1
+#         print("You Lose!")
+
+#     elif(computer == 1 and you == -1):  2
+#         print("You Lose!")
+
+#     elif(computer == 1 and you == 0):  1
+#         print("You Win!")    
+
+#     elif(computer == 0 and you == -1): 1
+#         print("You Win!")  
+
+
+#     elif(computer == 0 and you == 1): -1
+#         print("You Lose!")  
+
+#     else:
+#         print("Something went wrong!")
